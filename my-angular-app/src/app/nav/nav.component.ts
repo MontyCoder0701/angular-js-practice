9f27030c04c9011713dbed71dd5964db6f0d7659
Soo <soojlee0106@naver.com>
2023-08-19 23:41:46 +0900

    Add auth with logout

--- my-angular-app/src/app/nav/nav.component.ts
+++ my-angular-app/src/app/nav/nav.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Auth } from '../classes/auth';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+  authenticated = false;
+  constructor(private http: HttpClient) { }
 
+  ngOnInit(): void {
+    Auth.authEmitter.subscribe((authenticated: boolean) => {
+      this.authenticated = authenticated;
+    });
+  }
+
+  logout(): void {
+    this.http.post('http://localhost:3000/api/logout', {})
+      .subscribe(() => { });
+  }
 }
--- my-angular-app/src/app/pages/home/home.component.ts
+++ my-angular-app/src/app/pages/home/home.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Auth } from 'src/app/classes/auth';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  message = '';
 
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:3000/api/user')
+      .subscribe({
+        next: (res: any) => {
+          this.message = `Hello, ${res.first_name} ${res.last_name}!`;
+          Auth.authEmitter.emit(true);
+        },
+        error: (err) => {
+          this.message = `You are not logged in!`;
+          Auth.authEmitter.emit(false);
+        },
+      });
+  }
 }
